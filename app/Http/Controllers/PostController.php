a0f8d300fbb0528d452c5338e66d925de3196e8e
Dio-Rit <dionatan.ritter@universo.univates.br>
2024-03-08 11:51:35 -0300

    No meio do projeto

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdatePost;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+ public function index()
+ {
+    $posts = Post::orderBy('id', 'asc')->paginate(3);
+    return view("admin.posts.index", compact("posts"));
+ }
+
+public function create()
+{
+    return view("admin.posts.create");
+}
+
+public function store(StoreUpdatePost $request)
+{
+    Post::create($request->all());
+
+    return redirect()->route('posts.index')
+    ->with('message','Post criado com sucesso');
+}
+
+public function show($id)
+{
+    if (!$post = Post::find($id)){
+        return redirect()->route('posts.index');
+    };
+    
+    return view('admin.posts.show', compact('post'));
+}
+
+public function destroy($id)
+{
+    if (!$post = Post::find($id))
+        return redirect()->route('posts.index');
+            $post->delete();
+
+    return redirect()->route('posts.index')
+                     ->with('message','Post Deletado com sucesso');
+}
+
+public function edit($id)
+{
+    if (!$post = Post::find($id)){
+        return redirect()->back();
+    }
+    
+    return view('admin.posts.edit', compact('post'));
+}
+
+public function update(StoreUpdatePost $request ,$id)
+{
+    if (!$post = Post::find($id)){
+        return redirect()->back();
+    }
+
+    $post->update($request->all());
+
+    return redirect()->route('posts.index')
+                     ->with('message','Post atualizado com sucesso');
+}
+
+public function search(Request $request)
+{
+    $filters = $request->except('_token');
+
+    $posts = Post::where('title','=',"%{$request->search}%")
+                ->orWhere("content",'LIKE',"%{$request->search}%")
+                ->paginate();
+
+    return view('admin.posts.index', compact('posts', 'filters'));
+}
+
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,16 @@
+
+@extends('admin.layouts.app')
+
+@section('title', 'Criar Novo Post')
+
+@section('content')
+
+<h1>Cadastrar novo post</h1>
+
+<form action="{{ route('posts.store') }}" method="post">
+
+    @include('admin.posts._partials.form')
+
+</form>
+
+@endsection
